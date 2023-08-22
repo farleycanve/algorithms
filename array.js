af13e0a4641524e4ceea281e5cc18023b1c70e4a
farleycanve <baokhoi.vu@gmail.com>
2023-08-23 01:03:08 +0700

    Reversal

--- array.js
+++ array.js
@@ -10,3 +10,22 @@ const rotateArray1 = function (arr1, k) {
 
   return arr1;
 };
+//solution 1
+const rotateArray2 = function (nums, k) {
+  // reverse helper function
+  function reverse(arr, start, end) {
+    while (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
+  }
+
+  k %= nums.length;
+
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+
+  return nums;
+};
